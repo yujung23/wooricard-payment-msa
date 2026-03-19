a256246f563a24cfde972bdec3ba17dcd8dda923
FluanceIFI <gksrnr66@gmail.com>
2026-03-19 18:01:23 +0900

    ./docs 추가 및 .md 규칙 갱신 - h2 -> mysql-connector-j 변경

--- pay-pos-service/src/main/java/service/pos/PayPosServiceApplication.java
+++ pay-pos-service/src/main/java/service/pos/PayPosServiceApplication.java
@@ -2,8 +2,10 @@ package service.pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PayPosServiceApplication {
 
 	public static void main(String[] args) {
---
+++ pay-pos-service/src/main/java/service/pos/service/PaymentService.java
@@ -0,0 +1,4 @@
+package service.pos.service;
+
+public class PaymentService {
+}
