752ddd8d04d6d13e78e13ae413407fee11da58c1
FluanceIFI <gksrnr66@gmail.com>
2026-03-20 16:18:29 +0900

    vanclient, cardauthorizationclient @FeignClient 소문자 변경

--- pay-pos-service/src/main/java/com/card/payment/pos/client/VanClient.java
+++ pay-pos-service/src/main/java/com/card/payment/pos/client/VanClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "PAY-VAN-GATEWAY")
+@FeignClient(name = "pay-van-gateway")
 public interface VanClient {
 
     @PostMapping("/api/v1/approval/request")
